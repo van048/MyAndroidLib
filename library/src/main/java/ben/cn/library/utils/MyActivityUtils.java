6c25977fd45e3449c684c57ea693856292452d1f
dqshare <739362790@qq.com>
2016-12-16 16:37:28 +0800

    wrap ActivityUtils

--- library/src/main/java/ben/cn/library/utils/MyActivityUtils.java
+++ library/src/main/java/ben/cn/library/utils/MyActivityUtils.java
@@ -1,21 +1,18 @@
 package ben.cn.library.utils;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 
+import com.blankj.utilcode.utils.ActivityUtils;
+
 import static com.google.common.base.Preconditions.checkNotNull;
 
-/**
- * This provides methods to help Activities load their UI.
- */
 public class MyActivityUtils {
 
-    /**
-     * The {@code fragment} is added to the container view with id {@code frameId}. The operation is
-     * performed by the {@code fragmentManager}.
-     */
     public static void addFragmentToActivity(@NonNull FragmentManager fragmentManager,
                                              @NonNull Fragment fragment, int frameId) {
         checkNotNull(fragmentManager);
@@ -23,5 +20,21 @@ public class MyActivityUtils {
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.add(frameId, fragment);
         transaction.commit();
+    }
+
+    public static boolean isActivityExists(Context context, String packageName, String className) {
+        return ActivityUtils.isActivityExists(context, packageName, className);
+    }
+
+    public static void launchActivity(Context context, String packageName, String className) {
+        ActivityUtils.launchActivity(context, packageName, className);
+    }
+
+    public static void launchActivity(Context context, String packageName, String className, Bundle bundle) {
+        ActivityUtils.launchActivity(context, packageName, className, bundle);
+    }
+
+    public static String getLauncherActivity(Context context, String packageName) {
+        return ActivityUtils.getLauncherActivity(context, packageName);
     }
 }
